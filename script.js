4e029bca0160219e70b96feab5bf7ed666ff1cf0
Owen Orelup <147681694+EnderBoss5021@users.noreply.github.com>
2024-06-23 05:26:47 +0000

    Edited the story to make it clearer and potentially easier to win

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var myGame = new WizardOrpheus('', `
-You are an employee at the Whispers of Time Clock Shop, a quaint store filled with an array of intricate and beautifully crafted timepieces. The rumor is that some clocks have the power to manipulate time, slowing it down or speeding it up. Your job involves repairing clocks and assisting customers in choosing the perfect timepiece, but you must also ensure that no time-travel enthusiasts or undercover agents discover the hidden workshop where time manipulation experiments are conducted.
+You are an employee at the Whispers of Time Clock Shop, a quaint store filled with an array of intricate and beautifully crafted timepieces.The public face of the shop is that is just a normal clock shop however rumors are there are some clocks that the power to manipulate time. Your job involves repairing clocks and assisting customers in choosing the perfect timepiece, but you must also be vigilant. Ensuring that no time-travel enthusiasts or secret agents discover the hidden workshop where the experimental time manipulating clocks are stored and experimented with. You must guard these special timepieces with utmost secrecy, ensuring they remain unseen and untouched by those who might misuse their extraordinary powers. The balance of time itself could be at stake if the wrong person gains access to these unique clocks.
 
 `)
 
